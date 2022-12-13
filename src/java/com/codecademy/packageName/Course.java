85d4a6a36c9be5b8bb82e70c43d9d3143af0ff0b
unknown <snoet@rocketmail.com>
2022-12-13 11:57:57 +0100

    Added gitignore and addes basics of course

---
+++ src/java/com/codecademy/packageName/Course.java
@@ -0,0 +1,55 @@
+public class Course {
+    private int courseId;
+    private String title;
+    private String topic;
+    private String courseOwnerName;
+    private String description;
+
+    
+
+
+
+
+
+    //------------------ Default getters and setters
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getCourseOwnerName() {
+        return courseOwnerName;
+    }
+
+    public void setCourseOwnerName(String courseOwnerName) {
+        this.courseOwnerName = courseOwnerName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
