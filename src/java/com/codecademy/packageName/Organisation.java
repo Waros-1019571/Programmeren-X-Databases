005ace17fd31b0ebd81479630f70293fa301d0fd
Sno3t <tycho.brakenhoff@ictmbo.nl>
2022-12-13 14:49:19 +0100

    Made a basic organisation class, needs some updates

---
+++ src/java/com/codecademy/packageName/Organisation.java
@@ -0,0 +1,28 @@
+package src.java.com.codecademy.packageName;
+
+public class Organisation {
+    private int organisationId;
+    private String name;
+
+
+
+    //------------------ Default getters and setters
+
+    public int getOrganisationId() {
+        return courseId;
+    }
+
+    public void setOrganisationId(int organisationId) {
+        this.organisationId = organisationId;
+    }
+
+    public String getName() {
+        return title;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
