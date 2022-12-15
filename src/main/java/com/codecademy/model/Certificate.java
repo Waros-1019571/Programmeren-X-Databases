4a075ec5024b5d20be4f27877cc1033ee44e68ee
Daan de Vries <jumpsuitfrost@gmail.com>
2022-12-15 11:51:56 +0100

    Certificate

---
+++ src/main/java/com/codecademy/model/Certificate.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codecademy.model;
+
+/**
+ *
+ * @author Daan
+ */
+public class Certificate {
+
+    private int certificateId;
+    private double grade;
+
+    public Certificate(int certificateId, double grade) {
+        this.certificateId = certificateId;
+        this.grade = grade;
+    }
+
+    public int getCertificateId() {
+        return certificateId;
+    }
+
+    public void setCertificateId(int newCertificateId) {
+        this.certificateId = newCertificateId;
+    }
+//
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double newGrade) {
+        this.grade = newGrade;
+    }
+}
