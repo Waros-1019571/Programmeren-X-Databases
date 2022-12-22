0a885d7ed7618602fb39e2b386614215f0104f9e
unknown <snoet@rocketmail.com>
2022-12-22 14:30:16 +0100

    Testing adding an organisation

--- src/main/java/com/codecademy/MainTesting.java
+++ src/main/java/com/codecademy/MainTesting.java
@@ -1,8 +1,23 @@
 package com.codecademy;
 
 import com.codecademy.entity.Organisation;
+import com.codecademy.logic.DBConnection;
+import com.codecademy.model.OrganisationDAO;
+
+import java.sql.SQLException;
 
 public class MainTesting {
-    Organisation organisation  = new Organisation();
-    
+
+    public static void main(String[] args) {
+        DBConnection dbConnection = new DBConnection();
+        OrganisationDAO organisationDAO = new OrganisationDAO(dbConnection);
+        Organisation organisation = new Organisation();
+        organisation.setName("OrganisationName1");
+        try {
+            organisationDAO.create(organisation);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
