05ec62b94ab3a3ee3936c7e8c220ca2998594ae0
2205396 <ktl.li@student.avans.nl>
2022-12-22 04:46:50 +0100

    added a vertical slice

---
+++ src/main/java/com/codecademy/VerticalSliceApplication.java
@@ -0,0 +1,24 @@
+package com.codecademy;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class VerticalSliceApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("VerticalSlice-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        launch();
+    }
+}
