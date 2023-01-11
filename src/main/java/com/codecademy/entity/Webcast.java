938b9b0a0abdc991895749a67b772ddd954aeaaa
Kelvin Li <ktl.li@avans.student.nl>
2023-01-12 00:48:16 +0100

    cherry picked a file from #63f069ab

--- src/main/java/com/codecademy/entity/Webcast.java
+++ src/main/java/com/codecademy/entity/Webcast.java
@@ -1,31 +1,21 @@
 package com.codecademy.entity;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-
 import java.time.LocalDate;
 import java.sql.Date;
 import java.util.HashMap;
 
 public class Webcast {
-    private SimpleIntegerProperty id;
-    private SimpleStringProperty title;
-    private SimpleStringProperty description;
-    private SimpleStringProperty url;
-    private SimpleObjectProperty publicationDate;
-    private SimpleIntegerProperty duration;
+    private int id;
+    private String title;
+    private String description;
+    private String url;
+    private Date publicationDate;
+    private int duration;
     private VoiceActor voiceActor;
     private Course course;
     private HashMap<Student, Double> progressOfStudent = new HashMap<Student, Double>();
 
     public Webcast() {
-        id = new SimpleIntegerProperty();
-        title = new SimpleStringProperty();
-        description = new SimpleStringProperty();
-        url = new SimpleStringProperty();
-        duration = new SimpleIntegerProperty();
-        publicationDate = new SimpleObjectProperty();
     }
 
     public void addStudentProgress (Student student, Double progress) {
@@ -33,31 +23,31 @@ public class Webcast {
     }
 
     public int getDuration() {
-        return duration.get();
+        return duration;
     }
 
     public void setDuration(int duration) {
-        this.duration.set(duration);
+        this.duration = duration;
     }
 
     public LocalDate getPublicationDate() {
-        return (LocalDate) publicationDate.get();
+        return publicationDate.toLocalDate();
     }
 
     public void setPublicationDate(Date publicationDate) {
-        this.publicationDate.set(publicationDate);
+        this.publicationDate = publicationDate;
     }
 
     public void setPublicationDate(LocalDate publicationDate) {
-        this.publicationDate.set(Date.valueOf(publicationDate));
+        this.publicationDate = Date.valueOf(publicationDate);
     }
 
     public String getUrl() {
-        return url.get();
+        return url;
     }
 
     public void setUrl(String url) {
-        this.url.set(url);
+        this.url = url;
     }
 
     public VoiceActor getVoiceActor() {
@@ -69,27 +59,28 @@ public class Webcast {
     }
 
     public int getId() {
-        return id.get();
+        return id;
     }
 
     public void setId(int id) {
-        this.id.set(id);
+        this.id = id;
     }
 
     public String getTitle() {
-        return title.get();
+        return title;
     }
 
     public void setTitle(String title) {
-        this.title.set(title);
+        this.title = title;
     }
 
     public String getDescription() {
-        return description.get();
+        return description;
     }
 
     public void setDescription(String description) {
-        this.description.set(description);}
+        this.description = description;
+    }
 
     public Course getCourse() {
         return course;
